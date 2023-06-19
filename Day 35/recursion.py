ce9b985d10aac01e7b9c2017ae653f682754f436
Devanshe sharma <devanshesharma6@gmail.com>
2023-06-19 23:56:36 +0530

    Day 36
    Signed-off-by: Devanshe sharma <devanshesharma6@gmail.com>

--- Day 35/recursion.py
+++ Day 35/recursion.py
@@ -1,10 +1,7 @@
-class Solution:    
-    #Complete this function
-    def printNos(self,N,i=1):
-        #Your code here
-        if i==N:
-            print(N,end=" ")
-        else:
-            print(i,end=" ")
-            i=i+1
-            self.printNos(N,i)
+ def printNos(self,N,i=1):
+    if i==N:
+        print(N,end=" ")
+    else:
+        print(i,end=" ")
+        i=i+1
+        self.printNos(N,i)
